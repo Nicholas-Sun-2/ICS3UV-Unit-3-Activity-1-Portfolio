0f7989b8b769e73ed424cc6925da4642680434bf
default <nicholas.sun@stu.ocsb.ca>
2025-11-11 14:41:50 +0000

    Added the typescript program

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Nicholas Sun
+ * @version 1.0.0
+ * @date 2025-11-11
+ * @fileoverview This program calculates the sum of the numbers 10, -20, and 85.
+ */
+
+// Declaring variables
+const num1: number = 10;
+const num2: number = -20;
+const num3: number = 85;
+
+// Calculating the answer
+const answer: number = num1 + num2 + num3;
+
+// Printing
+console.log("10 + (-20) + 85 = " + String(answer));
